99ba3cc89e30ffc587ef8ad5fc2533f53b5a2e4e
Nagendra212121 <nagendranarayan1111@gmail.com>
2020-05-21 19:18:16 +0530

    Modified at 21-5-20 7:18pm

--- src/Components/Students/Studentlogin.js
+++ src/Components/Students/Studentlogin.js
@@ -4,9 +4,11 @@ import { RootContext } from "./../../RContext";
 import { Link } from "react-router-dom";
 import { MdVpnKey } from "react-icons/md";
 import { FaUniversity } from "react-icons/fa";
+import Axios from "axios";
 import {
   Form,
   FormGroup,
+  FormText,
   Label,
   InputGroup,
   InputGroupAddon,
@@ -18,11 +20,37 @@ export default class Studentlogin extends Component {
   static contextType = RootContext;
   state = {
     url: this.context.url,
+    usn: "",
+    pass: "",
+    failedmsg: "",
+  };
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+  submit = async (e) => {
+    try {
+      e.preventDefault();
+      if (this.state.usn === "" || this.state.pass === "") {
+        return this.setState({
+          failedmsg: "Invalid Usn or Password",
+        });
+      }
+      const { usn: studentUsn, pass: password } = this.state;
+      const res = await Axios.post(`${this.state.url}/student-login`, {
+        studentUsn,
+        password,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
   render() {
     return (
       <div className="login-container">
-        <div className="login-back border rounded-lg p-3 ">
+        <div className="login-back  p-3 ">
           <p className=" mx-2 text-black font-semibold uppercase hover:text-black hover:no-underline tracking-wider">
             clg website
           </p>
@@ -34,7 +62,10 @@ export default class Studentlogin extends Component {
           </Link>
         </div>
         <div className="login-content">
-          <Form className=" border bg-gray-200 rounded rounded-lg  hover:shadow-lg">
+          <Form
+            className=" border bg-gray-200 rounded rounded-lg  hover:shadow-lg"
+            onSubmit={this.submit}
+          >
             <h4 className="bg-red-500 text-gray-900 w-full p-3 rounded-lg uppercase tracking-widest mb-2">
               Login
             </h4>
@@ -48,7 +79,14 @@ export default class Studentlogin extends Component {
                     <FaUniversity className="text-red-500" />
                   </InputGroupText>
                 </InputGroupAddon>
-                <Input type="text" bsSize="lg" placeholder="1ga16is456" />
+                <Input
+                  type="text"
+                  name="usn"
+                  bsSize="lg"
+                  placeholder="1ga16is456"
+                  onChange={this.handleChange}
+                  autoComplete="off"
+                />
               </InputGroup>
             </FormGroup>
             <FormGroup className="mx-8 ">
@@ -61,11 +99,25 @@ export default class Studentlogin extends Component {
                     <MdVpnKey className="text-red-500" />
                   </InputGroupText>
                 </InputGroupAddon>
-                <Input type="password" bsSize="lg" placeholder="Password" />
+                <Input
+                  type="password"
+                  name="pass"
+                  bsSize="lg"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
               </InputGroup>
             </FormGroup>
+            <FormGroup className="mx-8 flex flex-wrap justify-center">
+              <FormText
+                color="danger"
+                className="text-lg font-semibold uppercase tracking-wide"
+              >
+                {this.state.failedmsg}
+              </FormText>
+            </FormGroup>
             <FormGroup className="flex justify-center">
-              <Button className="m-6  px-4 " type="submit" color="success">
+              <Button className="m-4  px-4 " type="submit" color="success">
                 Login
               </Button>
             </FormGroup>
