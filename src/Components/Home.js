4a41d7fc0ec07f95ec5b42d27385d4d850bd8f8a
Nagendra212121 <nagendranarayan1111@gmail.com>
2020-07-26 19:16:53 +0530

    modified

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
+import global from "./global.mp4";
+import logo from "./images/logo.png";
+import triback from "./images/triangle.jpg";
 export default class Home extends Component {
   state = {
     display: false,
@@ -8,25 +11,28 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <nav className=" flex justify-between md:m-8 p-3 md:border-2 rounded-lg">
-          <span className=" mx-2 text-black font-semibold uppercase hover:text-black hover:no-underline tracking-widest">
-            clg website
+        <nav className=" flex justify-between items-center md:m-8 p-3 md:border-2 rounded-lg ">
+          <span className="flex items-center">
+            <img src={logo} className="w-12 h-12" />
+            <span className=" mx-2 text-white font-semibold uppercase hover:text-black hover:no-underline tracking-widest">
+              gat website
+            </span>
           </span>
           <div className="home-nav">
             <Link
-              className="px-4 text-black font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
+              className="px-4 text-light font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
               to="staff-login"
             >
               Staff
             </Link>
             <Link
-              className="px-4 text-black font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
+              className="px-4 text-light font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
               to="cordinator-login"
             >
               Cordinator
             </Link>
             <Link
-              className="px-4 text-black font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
+              className="px-4 text-light font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
               to="student-login"
             >
               Student
@@ -34,7 +40,7 @@ export default class Home extends Component {
           </div>
           <div className="home-half">
             <button
-              className="mb-3 mx-2  py-1"
+              className="mb-3 mx-2   pt-3"
               onClick={() => {
                 this.setState({ display: !this.state.display });
               }}
@@ -49,7 +55,7 @@ export default class Home extends Component {
         </nav>
         <div className="md:hidden">
           <div
-            className={`h-40 mx-3  flex flex-col items-start px-3 border-2  bg-gray-200 rounded-lg shadow-sm `}
+            className={`h-40 mx-3 mb-2 flex flex-col items-start px-3  border-2  bg-gray-200 rounded-lg shadow-sm `}
             style={{
               display: this.state.display === true ? "flex" : "none",
             }}
@@ -79,6 +85,51 @@ export default class Home extends Component {
                 Student
               </Link>
             </div>
+          </div>
+        </div>
+        <div className="absolute  top-0 " style={{ zIndex: -2 }}>
+          <video
+            className="opacity-75  "
+            width="100%"
+            height="50%"
+            src={global}
+            autoPlay={true}
+            muted={true}
+            loop={true}
+          />
+
+          <div
+            className="p-4 border-2 rounded-lg "
+            style={{
+              backgroundImage: 'url("' + triback + '")',
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <span className="flex justify-center mb-2">
+              <span className="uppercase font-bold text-2xl text-gray-800 tracking-widest">
+                about gat
+              </span>
+            </span>
+            <p className="leading-loose h-64 text-gray-800 overflow-scroll md:ovgit erflow-visible font-semibold tracking-wider">
+              Global Academy of Technology, established in the year 2001, is one
+              of the most sought-after engineering and management colleges in
+              Bangalore, Karnataka. Located in a sprawling campus of 10 acre
+              land GAT has contemporary multi-media smart classrooms, modern
+              laboratories, seminar halls, auditorium, an up to date library
+              with volumes of recommended and reference books along with
+              e-journals and well equipped computer centre. Students @GAT
+              experience 24 x 7 high speed Wi-Fi facilities and web based CMS &
+              LMS for video streaming which is an integral part of e-learning.
+              GAT provides ample opportunities for various co-curricular and
+              extra-curricular activities for the students. A large cafeteria
+              and well maintained hostels for both boys and girls are available.
+              The campus brims with more than 3500 students and 300 experienced
+              staff involved in effective Teaching and Learning Process. Fully
+              established mentoring, peer learning and counseling by proctors
+              ensure holistic development of all students. GAT has academic
+              alliances with various industries and research organizations to
+              provide current industry perspective to the students.
+            </p>
           </div>
         </div>
       </div>
