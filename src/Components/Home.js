7454cdee376c65b43667a6d539de44506ef680a3
Nagendra212121 <nagendranarayan1111@gmail.com>
2020-07-27 00:17:51 +0530

    modified

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,8 @@ import "./../App.css";
 import global from "./global.mp4";
 import logo from "./images/logo.png";
 import triback from "./images/triangleb.jpg";
-import videoback from "./images/triangley.jpg";
+import videoback from "./images/triangler.jpg";
+import deptback from "./images/triangleg.jpg";
 export default class Home extends Component {
   state = {
     display: false,
@@ -13,28 +14,28 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <nav className=" flex justify-between items-center md:m-8 p-3 md:border-2 rounded-lg ">
+        <nav className=" flex justify-between items-center md:m-8 p-3 md:border-2 border-gray-700 rounded-lg ">
           <span className="flex items-center">
-            <img src={logo} className="w-12 h-12" />
-            <span className=" mx-2 text-white font-semibold uppercase hover:text-black hover:no-underline tracking-widest">
+            <img src={logo} alt="no logo" className="w-12 h-12" />
+            <span className=" mx-2 text-dark font-semibold uppercase hover:text-black hover:no-underline tracking-widest">
               gat website
             </span>
           </span>
           <div className="home-nav">
             <Link
-              className="px-4 text-light font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
+              className="px-4 text-gray-800 font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
               to="staff-login"
             >
               Staff
             </Link>
             <Link
-              className="px-4 text-light font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
+              className="px-4 text-gray-800 font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
               to="cordinator-login"
             >
               Cordinator
             </Link>
             <Link
-              className="px-4 text-light font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
+              className="px-4 text-gray-800 font-semibold uppercase  hover:text-black hover:no-underline tracking-widest"
               to="student-login"
             >
               Student
@@ -107,8 +108,8 @@ export default class Home extends Component {
             }}
           >
             <span className="flex justify-center mb-2">
-              <span className="uppercase font-bold text-2xl text-gray-800 tracking-widest">
-                about gat
+              <span className="uppercase font-bold text-2xl text-light tracking-widest">
+                about gat &#128515;
               </span>
             </span>
             <span>
@@ -139,35 +140,43 @@ export default class Home extends Component {
             </span>
 
             <div className="about-gat mt-4 ">
-              <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-pink-500 bg-gray-300   ">
+              <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-pink-500 bg-gray-300  hover:text-white hover:bg-pink-500 ">
                 <span className="flex justify-center font-bold  text-pink-500">
-                  <img src="https://img.icons8.com/dotty/80/000000/tuition.png" />
+                  <img
+                    src="https://img.icons8.com/dotty/80/000000/tuition.png"
+                    alt="nothing found"
+                  />
                 </span>
                 Providing Excellent Education Through High Quality, Experienced
                 and Committed Faculty.
               </p>
-              <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-pink-500 bg-gray-300  ">
+              <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-pink-500 bg-gray-300  hover:text-white hover:bg-pink-500 ">
                 <span className="flex justify-center font-bold  text-pink-500">
                   <img
                     className="my-2"
                     src="https://img.icons8.com/wired/64/000000/book-shelf.png"
+                    alt="nothing found"
                   />
                 </span>
                 Evolving creative processes for optimal Knowledge and Skill
                 Transfer.
               </p>
-              <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-pink-500 bg-gray-300  ">
+              <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-pink-500 bg-gray-300  hover:text-white hover:bg-pink-500 ">
                 <span className="flex justify-center font-bold  text-pink-500">
-                  <img src="https://img.icons8.com/dotty/80/000000/student-center.png" />
+                  <img
+                    src="https://img.icons8.com/dotty/80/000000/student-center.png"
+                    alt="nothing found"
+                  />
                 </span>
                 Building up state-of-the-art infrastructure at par with
                 International standards.
               </p>
-              <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-pink-500 bg-gray-300  ">
+              <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-pink-500 bg-gray-300   hover:text-white hover:bg-pink-500">
                 <span className="flex justify-center font-bold  text-pink-500">
                   <img
                     className="my-2"
                     src="https://img.icons8.com/pastel-glyph/64/000000/microscope.png"
+                    alt="nothing found"
                   />
                 </span>
                 Creating an environment for holistic personality development and
@@ -187,7 +196,7 @@ export default class Home extends Component {
                   vision
                 </p>
                 <span className="about-gat">
-                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-yellow-500 bg-gray-300  ">
+                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-orange-500 bg-gray-300  hover:text-white hover:bg-orange-500 ">
                     Become a premier institution imparting quality education in
                     engineering and management to meet the changing needs of
                     society.
@@ -199,16 +208,16 @@ export default class Home extends Component {
                   mission
                 </p>
                 <span className="about-gat">
-                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-yellow-500 bg-gray-300  ">
+                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-green-500 bg-gray-300 hover:text-white hover:bg-green-500 ">
                     Create environment conducive for continuous learning through
                     quality teaching and learning processes supported by modern
                     infrastructure.
                   </p>
-                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-yellow-500 bg-gray-300  ">
+                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-green-500 bg-gray-300 hover:text-white hover:bg-green-500 ">
                     Promote Research and Innovation through collaboration with
                     industries.
                   </p>
-                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-yellow-500 bg-gray-300  ">
+                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-green-500 bg-gray-300 hover:text-white hover:bg-green-500 ">
                     Inculcate ethical values and environmental consciousness
                     through holistic education programs.
                   </p>
@@ -217,13 +226,14 @@ export default class Home extends Component {
             </div>
             <div className="my-4">
               <p className="text-center tracking-widest text-lg font-bold text-white uppercase">
-                watch us here!!
+                watch us here &#128521;!!
               </p>
               <span className="flex justify-center ">
                 <iframe
                   className="md:ml-4"
                   width="90%"
                   height="360"
+                  title="gat video"
                   src="https://www.youtube.com/embed/54PaHnh9Pg0"
                   frameborder="0"
                   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
@@ -232,6 +242,95 @@ export default class Home extends Component {
               </span>
             </div>
           </div>
+          <div></div>
+          <div
+            style={{
+              backgroundImage: 'url("' + deptback + '")',
+              backgroundPosition: "60%",
+            }}
+          >
+            <span className="flex justify-center py-2">
+              <p className="uppercase font-bold text-2xl text-light tracking-widest">
+                about ise &#128525;
+              </p>
+            </span>
+            <span className=" flex flex-col md:flex-row justify-between">
+              <span>
+                <p className="text-center tracking-widest text-lg font-bold text-white uppercase py-2">
+                  department vision
+                </p>
+                <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-indigo-500 bg-gray-300 hover:text-white hover:bg-indigo-500 ">
+                  To excel in teaching and research in the field of Information
+                  Science and Engineering to meet emerging challenges of
+                  society.
+                </p>
+              </span>
+              <span>
+                <p className="text-center tracking-widest text-lg font-bold text-white uppercase py-2">
+                  department mission
+                </p>
+                <span className="flex  flex-col md:flex-row justify-evenly">
+                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-red-500 bg-gray-300  hover:text-white hover:bg-red-500">
+                    To inculcate strong academic foundation in the Information
+                    Technology domain for successful career and lifelong
+                    learning.
+                  </p>
+                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-red-500 bg-gray-300 hover:text-white hover:bg-red-500 ">
+                    To strengthen research and development activities through
+                    interaction with Industry.
+                  </p>
+                  <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-red-500 bg-gray-300 hover:text-white hover:bg-red-500">
+                    To instill professional ethics and social values with
+                    concern for environment.
+                  </p>
+                </span>
+              </span>
+            </span>
+            <span>
+              <p className="text-center tracking-widest text-lg font-bold text-white uppercase py-3">
+                Program Educational Objectives
+              </p>
+              <span className="about-dept">
+                <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-purple-600 bg-gray-300 hover:text-white hover:bg-purple-600">
+                  Identify and develop IT solutions to solve societal problems.
+                </p>
+                <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-purple-600 bg-gray-300  hover:text-white hover:bg-purple-600">
+                  Accelerate career path or engage in entrepreneurship with
+                  leadership qualities, Professional ethics, soft skills and
+                  teamwork.
+                </p>
+                <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-purple-600 bg-gray-300 hover:text-white hover:bg-purple-600">
+                  Engage in improving professional knowledge through continuing
+                  education and research in engineering or management.
+                </p>
+              </span>
+            </span>
+            <span>
+              <p className="text-center tracking-widest text-lg font-bold text-white uppercase py-3">
+                Program Specific Outcomes
+              </p>
+              <span className="about-dept">
+                <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-yellow-500 bg-gray-300 hover:text-white hover:bg-yellow-500">
+                  Analyze, develop, debug and test application software to meet
+                  specified requirements.
+                </p>
+                <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-yellow-500 bg-gray-300 hover:text-white hover:bg-yellow-500">
+                  Understand and analyze the system architecture, organization
+                  of computer communication systems.
+                </p>
+                <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-yellow-500 bg-gray-300 hover:text-white hover:bg-yellow-500">
+                  Understand the features of System Software in Information
+                  Systems.
+                </p>
+                <p className="font-semibold tracking-wide leading-relaxed p-2  rounded-lg mx-6 border-b-4 border-yellow-500 bg-gray-300 hover:text-white hover:bg-yellow-500">
+                  Design and develop applications for Information security.
+                </p>
+              </span>
+            </span>
+          </div>
+          {
+            //jdsfa;l
+          }
         </div>
       </div>
     );
